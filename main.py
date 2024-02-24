99698486855c676bace0112d8299e23d90937f68
25prajwal <94011967+25prajwal@users.noreply.github.com>
2024-02-25 02:14:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request, send_file
+import PyPDF2
 import os
 
 app = Flask(__name__)
@@ -7,7 +8,48 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/Cheatpdf_work', methods = ['POST'])
+def split_pdf():
+   if request.method == 'POST':
+        f = request.files['file']
+        f.save("node/"+f.filename)
+        pdf_file = open("node/"+f.filename, 'rb')
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        pdf_output = PyPDF2.PdfWriter()
+        k=0
+        s=9 #No. of page per Page
+        nPage=len(pdf_reader.pages ) #no. of page
+        for i in range(s*2,(int(nPage/18)*18)+1,s*2):
+            for j in range(k,i,2):
+                print("==========>",j)
+                pdf_output.add_page(pdf_reader.pages[j])
+            for j in range(k+1,i+1,2):
+                pdf_output.add_page(pdf_reader.pages[j])
+                print("------------------------------>",j)
+            k=i
+        oPage=int(nPage%18)
+        if oPage:
+            for i in range(0,(oPage),2):
+                print("==========>------------",i+k)
+                pdf_output.add_page(pdf_reader.pages[i+k])
+            for i in range(10-round(oPage/2)):
+                pdf_output.add_blank_page()
+            for i in range(1,(oPage),2):
+                pdf_output.add_page(pdf_reader.pages[i+k])
 
+        with open("node/"+f.filename,"wb") as (out):
+            pdf_output.write(out)
+        return send_file("node/"+f.filename,mimetype='application/pdf')
+   return "no file"
+
+@app.route('/Cheatpdf')
+def pdfGet():
+    html='''<form action = "/Cheatpdf_work" method = "post" enctype="multipart/form-data">
+        <input type="file" name="file" />
+        <input type = "submit" value="Upload">
+    </form>
+    '''
+    return html
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
